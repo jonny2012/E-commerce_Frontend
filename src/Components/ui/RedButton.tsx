885b270f854dcfef9f28c97a4978ebe3d9b93b7d
Jonny <vanea.ss21@gmail.com>
2024-12-19 14:19:24 +0100

    fix dependency versions

--- src/Components/ui/RedButton.tsx
+++ src/Components/ui/RedButton.tsx
@@ -1,17 +1,18 @@
-import { FC } from "react"
-import styles from "./Ui.module.scss"
+import { FC } from "react";
+import styles from "./Ui.module.scss";
 
-interface IButonText{
-    words:string;
-    width:string;
+interface IButonText {
+  words: string;
+  width: string;
 }
 
-
- export const RedButton:FC<IButonText> = ({words, width})=>{
-
-    return (
-        <button style={{width: width, color:"#fff"}} className={styles.red_button}>
-        {words}
-        </button>
-    )
-}
+export const RedButton: FC<IButonText> = ({ words, width }) => {
+  return (
+    <button
+      style={{ width: width, color: "#fff" }}
+      className={styles.red_button}
+    >
+      {words}
+    </button>
+  );
+};
